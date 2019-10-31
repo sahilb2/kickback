88143dc37e36c0048e83f58485cb742454ac72d2
Ryan Kastl <ryankastl16@gmail.com>
2019-10-30 23:31:23 -0500

    Added utilization of getQueue in views.py

--- kickback/apps/core/views.py
+++ kickback/apps/core/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.http import require_http_methods
 import json
 from kickback.apps.core.manager.search import search_tracks_by_query
+from kickback.apps.core.manager.getQueue import get_tracks_in_queue
 
 from .models import User, Sessions, SessionSongs
 
@@ -26,7 +27,8 @@ def delete_song(request):
     return HttpResponse('Delete Song Endpoint')
 
 def get_queue(request):
-    query = request.GET.get('q')
-    if query is None or query == '':
-        return HttpResponseBadRequest('Use paramter \'q\' to specify query for the search')
-    return HttpResponse('Get Queue Endpoint')
+    sessionId = request.GET.get('q')
+    if sessionId is None or sessionId == '':
+        return HttpResponseBadRequest('Use paramter \'q\' to specify sessionId for the queue')
+    tracks = get_tracks_in_queue(sessionId)
+    return HttpResponse(json.dumps(tracks), content_type='application/json')
