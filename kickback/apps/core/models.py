9562c2cd17aa994ca16954de9041475084761168
sahilb2 <sahilb2@illinois.edu>
2019-12-03 18:37:39 -0600

    Allowing non-blank values to be null for Django models - recommended use case

--- kickback/apps/core/models.py
+++ kickback/apps/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Sessions(models.Model):
     session_id = models.CharField(max_length=4, primary_key=True, verbose_name='Session ID')
     session_name = models.CharField(max_length=64, blank=True, null=True, verbose_name='Session Name')
-    owner = models.CharField(max_length=20, blank=False, verbose_name='Session Owner')
+    owner = models.CharField(max_length=20, blank=False, null=True, verbose_name='Session Owner')
     session_pass = models.CharField(max_length=16, blank=True, null=True, verbose_name='Session Password')
 
 class SessionSongs(models.Model):
@@ -11,7 +11,7 @@ class SessionSongs(models.Model):
     spotify_uri = models.CharField(max_length=22, blank=False, verbose_name='Song URI')
     next_song_id = models.IntegerField(blank=True, null=True, verbose_name='Next Song ID')
     session = models.ForeignKey(Sessions, on_delete=models.CASCADE, verbose_name='Session ID')
-    username = models.CharField(max_length=20, blank=False, verbose_name='Added By User')
+    username = models.CharField(max_length=20, blank=False, null=True, verbose_name='Added By User')
 
 class CurrentSongs(models.Model):
     session = models.OneToOneField(Sessions, on_delete=models.CASCADE, verbose_name='Session ID')
