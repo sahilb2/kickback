c37aa2092561991cb119791d8618fd02d2c44a5f
sahilb2 <sahilb2@illinois.edu>
2019-12-05 11:18:32 -0600

    Added socket call for the chat

--- kickback/apps/core/manager/chat.py
+++ kickback/apps/core/manager/chat.py
@@ -1,13 +1,18 @@
 import json
+import requests
 from kickback.apps.core.models import ChatMessages
 from django.db import transaction, connection
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
+
+def call_socket_for_chat_update(session_id):
+    requests.post('https://kickback-socket-server.herokuapp.com/updateChat', {'sessionid': session_id})
 
 @transaction.atomic
 def add_to_chat_for_session(session_id, message, username):
     with connection.cursor() as cursor:
         cursor.execute('INSERT INTO core_chatmessages(session_id, message, username) VALUES (%s, %s, %s)',
             [session_id, message, username])
+    call_socket_for_chat_update(session_id)
     return HttpResponse('Chat has been added')
 
 def retrieve_chat_for_session(session_id):
